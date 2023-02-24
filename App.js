ff2ffde9e005ca805c5b8714e98e5dc1957ed64a
Gustavo Januário <gustavo.sjanuario@gmail.com>
2023-02-24 12:12:56 -0300

    trabalho tela de noticias

--- App.js
+++ App.js
@@ -1,51 +1,51 @@
-import React from "react";
-import { View, Text, Image, SafeAreaView, Button, ScrollView } from 'react-native';
 
-function App() {
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import styles from './styles';
 
+const App = () => {
   return (
+    <ScrollView style={styles.scrollView}>
+      {/* Primeira notícia */}
+      <View style={styles.view}>
+        <Text style={styles.title}>Curiosidade para o dia 23 de fevereiro</Text>
+        <Image 
+          style={styles.image}
+          source={{uri: 'https://cbltech.com.br/uploads/editor/27715b3856c5fe88c20ca8ceab3ed28e.jpg'}}
+        />
+        <Text style={styles.text}>Em 2005, era descoberto o Cabir, o primeiro vírus para telefones celulares, infectando o sistema Symbian.</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Continuar Leitura</Text>
+        </TouchableOpacity>
+      </View>
 
-    // View Principal
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#f0f0f0',
-    }}>
-      <SafeAreaView>
-        <ScrollView>
+      {/* Segunda notícia */}
+      <View style={styles.view}>
+        <Text style={styles.title}>Para cientista-chefe de IA da Meta, aprendizado de máquina teria avançado mais rapidamente se outra linguagem tivesse sido adotada em vez de Python</Text>
+        <Image 
+          style={styles.image}
+          source={{uri: 'https://dkrn4sk0rn31v.cloudfront.net/uploads/2020/01/07093212/PYTHON.png'}}
+        />
+        <Text style={styles.text}>Em seu perfil no Twitter, o engenheiro Yann LeCun afirma que o ideal seria uma linguagem interativa e ao mesmo tempo rápida e compilável, como Julia ou da família Lisp. Na Meta, os esforços iniciais de IA começaram com Lua, mas os engenheiros “odiavam” trabalhar com a linguagem.</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Continuar Leitura</Text>
+        </TouchableOpacity>
+      </View>
 
-          <View style={{
-            paddingTop:20,
-          }}>
-
-            <Text style={{ fontSize: 20, marginBottom: 10, color: '#000000' }}>
-
-              Este é meu primeiro projeto!
-              {'\n'}
-              Hoje é dia 17.
-
-
-            </Text>
-
-            <Image
-              style={{ width: 100, height: 100, borderRadius: 50, marginBottom: 10 }}
-              source={{
-                uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPDmo0NOFOJKfeb64hUIbmLBzT_jqtMSgujg&usqp=CAU',
-              }}
-            />
-            <Button style={{with:100, height:100, paddingTop:50}} title="OK"></Button>
-          </View>
-
-
-        </ScrollView>
-      </SafeAreaView>
-    </View>
-
-
+      {/* Terceira notícia */}
+      <View style={styles.view}>
+        <Text style={styles.title}>OpenAI já está testando IA jurídica em escritórios de advocacia nos EUA</Text>
+        <Image 
+          style={styles.image}
+          source={{uri: 'https://dunaglobaltimes.no/wp-content/uploads/2023/02/DGT_OpenAI.jpg'}}
+        />
+        <Text style={styles.text}>A ferramenta Harvey obteve ampla adesão das equipes na pesquisa e escrita de documentos legais. Para David Wakeling, chefe de um escritório de advocacia londrino, a tecnologia representa o início de uma “mudança de paradigma” na indústria jurídica. As informações são do site Wired.</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Continuar Leitura</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
-
-
 }
 
 export default App;
